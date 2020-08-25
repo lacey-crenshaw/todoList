0d6a17ae916e8f56a5caceca5ae0a1f650d6cfa5
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-08-25 10:38:01 -0600

    script.js bug fix

--- js/script.js
+++ js/script.js
@@ -44,3 +44,43 @@ function renderTodos(todos) {
         todoItemsList.append(li);
     });
 }
+
+function addToLocalStorage(todos) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderTodos(todos);
+}
+
+function getFromLocalStorage() {
+    const reference = localStorage.getItem('todos');
+    if (reference) {
+        todos = JSON.parse(reference);
+        renderTodos(todos);
+    }
+}
+
+getFromLocalStorage();
+
+todoItemsList.addEventListener('click', function (event) {
+    if (event.target.type === 'checkbox') {
+        toggle(event.target.parentElement.getAttribute('data-key'));
+        deleteTodo(event.target.parentElement.getAttribute('data-key'));
+    }
+});
+
+function toggle(id) {
+    todos.forEach(function (item) {
+        if (item.id == id) {
+            item.completed = !item.completed;
+        }
+    });
+
+    addToLocalStorage(todos);
+}
+
+function deleteTodo(id) {
+    todos = todos.filter(function (item) {
+        return item.id != id;
+    });
+
+    addToLocalStorage(todos);
+}
