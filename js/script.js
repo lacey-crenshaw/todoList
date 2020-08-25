ca5dedce316343a0eb62be9fa6a0a93191a98079
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-08-25 10:11:07 -0600

    script.js creation

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input')
+const todoItemsList = document.querySelector('.todo-items')
+
+let todos = [];
+
+todoForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addTodo(todoInput.value);
+});
+
+function addTodo(item) {
+    if (item !== '') {
+        const todo = {
+            id: Date.now(),
+            name: item;
+            completed: false;
+        };
+
+        todos.push(todo);
+        renderTodos(todos);
+        todoInput.value = '';
+    }
+}
+
+function renderTodos(todos) {
+    todoItemsList.innerHTML = '';
+    todos.forEach(function (item) {
+        const checked = item.completed ? 'checked' : null;
+        const li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        li.setAttribute('data-key', item.id);
+
+        if (item.completed === true) {
+            li.classList.add('checked');
+        }
+
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox" ${checked}>
+        ${item.name}
+        <button class="delete-button">💣</button>
+        `;
+
+        todoItemsList.append(li);
+    });
+}
